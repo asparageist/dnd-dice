8189ea400fa0830376fb44d5485bbcb4552aaf8b
Joseph Murray <josephwmurray@gmail.com>
2023-02-20 19:46:59 -0800

    add styles to css, clean up comments

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,3 @@
-// Business Logic
-
-// Define the game state
 const Players = {
   "player-1-score": 0,
   "player-2-score": 0,
@@ -11,7 +8,7 @@ const Players = {
 };
 
 let currentPlayerId =  document.getElementById("player-selector").value;
-////new one
+
 function rollDice() {
   const diceType = parseInt(document.getElementById("dice-type").value);
   const diceResult = Math.floor(Math.random() * diceType) + 1;
@@ -21,25 +18,6 @@ function rollDice() {
   setNextPlayer();
 }
 
-// function setNextPlayer(playerIndex) {
-//   if (playerIndex === 6) {
-//     currentPlayerId = "player-1-score";
-//   } else {
-//     currentPlayerId = `player-${playerIndex + 1}-score`;
-//   }
-//   const selectedPlayerScore = Players[currentPlayerId];
-//   document.querySelectorAll("tr").forEach((tr) => tr.classList.remove("selected"));
-//   document.getElementById(currentPlayerId).parentNode.classList.add("selected");
-// }
-
-// document.querySelectorAll("tr").forEach((tr, index) => {
-//   tr.addEventListener("click", () => {
-//     const playerId = tr.children[1].id.split("-")[1];
-//     setNextPlayer(playerId);
-//   });
-// });
-
-
 function setNextPlayer() {
   const currentPlayerIndex = Number(currentPlayerId.split("-")[1]);
   if (currentPlayerIndex === 6) {
@@ -48,7 +26,7 @@ function setNextPlayer() {
     currentPlayerId = `player-${currentPlayerIndex + 1}-score`;
   }
 }
-///new one
+
 function selectPlayer(playerId) {
   currentPlayerId = `player-${playerId}-score`;
   const selectedPlayerScore = Players[currentPlayerId];
